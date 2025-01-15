89cfe4c05e6cfc8f23bb47172b56a037f7ae0741
CITWil <93660053+CITWil@users.noreply.github.com>
2025-01-15 11:06:43 -0500

    Update Win11sddm.sh
    declare input var
    clear sudo

--- files/scripts/Win11sddm.sh
+++ files/scripts/Win11sddm.sh
@@ -10,18 +10,19 @@ echo " (3) - Install Windows Cursor Icons"
 echo ""
 
 echo "Installing all: (e.g: "1 2")"
-   
+
+input = "1 2 3"
 #read -p ":: " input
 
 function sddm() {
 
-    sudo wget -P /usr/share/sddm/themes https://github.com/birbkeks/win11-sddm-theme/releases/download/1.0/win11-sddm-theme.tar.gz -nc
+    wget -P /usr/share/sddm/themes https://github.com/birbkeks/win11-sddm-theme/releases/download/1.0/win11-sddm-theme.tar.gz -nc
 
-    sudo tar -xzf /usr/share/sddm/themes/win11-sddm-theme.tar.gz -C /usr/share/sddm/themes
+    tar -xzf /usr/share/sddm/themes/win11-sddm-theme.tar.gz -C /usr/share/sddm/themes
 
-    sudo rm -rf /usr/share/sddm/themes/win11-sddm-theme.tar.gz
+    rm -rf /usr/share/sddm/themes/win11-sddm-theme.tar.gz
 
-    sudo rm -rf /usr/share/sddm/themes/win11-sddm-theme/.git/
+    rm -rf /usr/share/sddm/themes/win11-sddm-theme/.git/
 
     #edit
 }
@@ -50,27 +51,27 @@ Current=win11-sddm-theme
 function font() {
     # Not installed locally because I can't stand to see this font on Github and other websites, I couldn't find a way to disable this font for browser. I'm used to see Noto Sans too much I guess. You can go to this fonts files and install it locally if you want to and this theme will still work.
 
-    sudo wget -P /usr/share/sddm/themes/win11-sddm-theme/fonts https://github.com/birbkeks/win11-sddm-theme/raw/main/fonts/Segoe-UI-Variable-Static-Display.ttf -nc
+    wget -P /usr/share/sddm/themes/win11-sddm-theme/fonts https://github.com/birbkeks/win11-sddm-theme/raw/main/fonts/Segoe-UI-Variable-Static-Display.ttf -nc
 
-    sudo wget -P /usr/share/sddm/themes/win11-sddm-theme/fonts https://github.com/birbkeks/win11-sddm-theme/raw/main/fonts/Segoe-UI-Variable-Static-Display-Semibold.ttf -nc
+    wget -P /usr/share/sddm/themes/win11-sddm-theme/fonts https://github.com/birbkeks/win11-sddm-theme/raw/main/fonts/Segoe-UI-Variable-Static-Display-Semibold.ttf -nc
 
-    sudo wget -P /usr/share/sddm/themes/win11-sddm-theme/fonts https://github.com/birbkeks/win11-sddm-theme/raw/main/fonts/Segoe-Fluent-Icons.ttf -nc
+    wget -P /usr/share/sddm/themes/win11-sddm-theme/fonts https://github.com/birbkeks/win11-sddm-theme/raw/main/fonts/Segoe-Fluent-Icons.ttf -nc
 
-    sudo wget -P /usr/share/sddm/themes/win11-sddm-theme/fonts https://github.com/birbkeks/win11-sddm-theme/raw/main/fonts/SegoeBoot-Semilight.woff -nc
+    wget -P /usr/share/sddm/themes/win11-sddm-theme/fonts https://github.com/birbkeks/win11-sddm-theme/raw/main/fonts/SegoeBoot-Semilight.woff -nc
 }
 
 function cursor() {
-    sudo wget -P /usr/share/icons https://github.com/birbkeks/windows-cursors/releases/download/1.0/windows-cursors.tar.gz -nc
+    wget -P /usr/share/icons https://github.com/birbkeks/windows-cursors/releases/download/1.0/windows-cursors.tar.gz -nc
 
-    sudo tar -xzf /usr/share/icons/windows-cursors.tar.gz -C /usr/share/icons/
+    tar -xzf /usr/share/icons/windows-cursors.tar.gz -C /usr/share/icons/
 
-    sudo cp /usr/share/icons/windows-cursors/index.theme /usr/share/icons/default/index.theme
+    cp /usr/share/icons/windows-cursors/index.theme /usr/share/icons/default/index.theme
 
-    sudo cp -r /usr/share/icons/windows-cursors/cursors /usr/share/icons/default
+    cp -r /usr/share/icons/windows-cursors/cursors /usr/share/icons/default
 
-    sudo rm -rf /usr/share/icons/windows-cursors
+    rm -rf /usr/share/icons/windows-cursors
 
-    sudo rm -f /usr/share/icons/windows-cursors.tar.gz
+    rm -f /usr/share/icons/windows-cursors.tar.gz
 }
 
 if [[ $input == "1" ]]; then
