7701452845e4eab2d66a2299188758b6ed562766
CITWil <wmaxwell@communityit.com>
2025-04-14 02:31:02 -0400

    edge take 3.2

--- files/scripts/MSLocal-build.sh
+++ files/scripts/MSLocal-build.sh
@@ -3,12 +3,12 @@
 set -ouex pipefail
 
 ### Install packages
-#curl -L -o /etc/yum.repos.d/microsoft-rhel9.0-prod.repo https://packages.microsoft.com/yumrepos/microsoft-rhel9.0-prod/config.repo
-#curl -o /etc/yum.repos.d/microsoft-edge.repo https://packages.microsoft.com/yumrepos/edge/config.repo
+curl -L -o /etc/yum.repos.d/microsoft-rhel9.0-prod.repo https://packages.microsoft.com/yumrepos/microsoft-rhel9.0-prod/config.repo
+curl -o /etc/yum.repos.d/microsoft-edge.repo https://packages.microsoft.com/yumrepos/edge/config.repo
 
 # Undo the work of good open source maintainers https://github.com/ublue-os/bluefin-lts/pull/425
 # Required because the Microsoft packages are an absolute joke
-rm /opt && mkdir /opt && mkdir /var/opt
+rm /opt && mkdir /opt #&& mkdir /var/opt
 dnf install -y intune-portal microsoft-edge-stable
 mv /opt/microsoft /usr/lib/opt/microsoft
 ln -s /usr/lib/opt/microsoft /var/opt/microsoft
