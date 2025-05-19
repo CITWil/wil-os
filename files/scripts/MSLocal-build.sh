b739fce5be5ccb5cf8787e6c08662dba865ddba8
CITWil <wmaxwell@communityit.com>
2025-05-18 23:47:53 -0400

    disable Intune portal install

--- files/scripts/MSLocal-build.sh
+++ files/scripts/MSLocal-build.sh
@@ -9,7 +9,7 @@ curl -o /etc/yum.repos.d/microsoft-edge.repo https://packages.microsoft.com/yumr
 # Undo the work of good open source maintainers https://github.com/ublue-os/bluefin-lts/pull/425
 # Required because the Microsoft packages are an absolute joke
 rm /opt && mkdir /opt #&& mkdir /var/opt
-dnf install -y intune-portal microsoft-edge-beta
+dnf install -y microsoft-edge-beta
 mv /opt/microsoft /usr/lib/opt/microsoft
 ln -s /usr/lib/opt/microsoft /var/opt/microsoft
 ls -l /var/opt
